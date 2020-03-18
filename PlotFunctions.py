a81101e068a701e6745f43d2b38ee436a9e4bb2f
remedeiw <maximilian.wiedemer@gmail.com>
2020-03-18 19:51:31 +0100

    Data Entry Control Market

---
+++ PlotFunctions.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def PlotDataFrameControlEnergy(dataframe,posorneg):
+    dataframegroup = dataframe.groupby('PRODUCT')
+    plt.figure(figsize = (12, 5))
+    for product, group in dataframegroup:
+        if product[:3] == posorneg:
+            group['TOTAL_AVERAGE_CAPACITY_PRICE_[EUR/MW]'].plot()
+    legend = plt.legend(loc='upper left', frameon=False)
+    legend.get_texts()[0].set_text(posorneg + '_00_04')
+    legend.get_texts()[1].set_text(posorneg +'_04_08')
+    legend.get_texts()[2].set_text(posorneg +'_08_12')
+    legend.get_texts()[3].set_text(posorneg +'_12_16')
+    legend.get_texts()[4].set_text(posorneg +'_16_20')
+    legend.get_texts()[5].set_text(posorneg +'_20_24')
+    plt.xticks(rotation=45)
+
+    
